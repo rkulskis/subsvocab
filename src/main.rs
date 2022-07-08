04760ed7cb7908f53da0154233ef53099dd1c35a
rkulskis <rkulskis@gmail.com>
2022-07-07 21:20:40 -0400

    ran with german_subs file, works

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,5 @@ pub fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     // log!("Hello World!");
-    parser::parse_subs("ipv4-c093-bos001-ix.1.oca.nflxvideo.net");
+    parser::parse_subs("german_subs");
 }
